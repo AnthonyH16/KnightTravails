11b82bbf402368193b841702312eac8f5b7f6054
AnthonyH16 <132026902+AnthonyH16@users.noreply.github.com>
2025-03-02 15:36:08 -0500

    Use BFS to find shortest path for knight to get from starting position to end position.

--- knightTravails.js
+++ knightTravails.js
@@ -1,14 +1,3 @@
-// class Vertex{
-//     constructor(coordinate){
-//         this.coordinate = coordinate;
-//     }
-// }
-
-// class Graph{
-//     constructor(){
-//         this.startPos = null;
-//     }
-
 function createAdjList(startPos){
         const adjList = {};
         const possibleMoves = [
@@ -34,12 +23,50 @@ function createAdjList(startPos){
             }
         }
         console.log(adjList);
+        console.log(typeof(adjList));
         return adjList;
     }
-adfsdafasdf
-    function findNextMoves(adjList){
 
+function isVisited(visited, position) {
+    return visited.indexOf(position) !== -1;
+}
+
+function knightMoves(startPos, endPos) {
+    const start = `${startPos[0]},${startPos[1]}`;
+    const end = `${endPos[0]},${endPos[1]}`;
+
+    const adjList = createAdjList();
+    
+    const queue = [[start]]; // Queue for BFS, starts with the starting position
+    const visited = []; // Array to track visited nodes
+    
+    while (queue.length > 0) {
+        const path = queue.shift(); // Get the first path from the queue
+        const position = path[path.length - 1]; // Get the last position from the path
+        
+        // Check if we have reached the end position
+        if (position === end) {
+            console.log(`You made it in ${path.length - 1} moves! Here's your path:`);
+            path.forEach(pos => console.log(`[${pos.split(',')[0]},${pos.split(',')[1]}]`));
+            return;
+        }
+
+        // Mark this position as visited
+        if (!isVisited(visited, position)) {
+            visited.push(position); // Add position to the visited array
+            
+            // Explore neighbors
+            for (const neighbor of adjList[position]) {
+                if (!isVisited(visited, neighbor)) {
+                    const newPath = [...path, neighbor]; // Create a new path
+                    queue.push(newPath); // Add the new path to the queue
+                }
+            }
+        }
     }
 
-createAdjList([0,0]);
-// }
+    console.log("There's no possible path to the end position.");
+}
+
+// Example Usage
+knightMoves([3, 3], [4, 3]);
